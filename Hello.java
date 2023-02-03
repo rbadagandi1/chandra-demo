c12ba0a150090841f024a2c04da8957bd008d931
Chandrakala M Subramanyam <csubrama@redhat.com>
2023-02-03 16:53:51 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 class Hello {
   public static void main (string[] args){
+      System.out.println("Hello World"); 
       System.out.println("Hello from chandra"); 
+      System.out.println("Hello from kruize"); 
   }
   
 }
