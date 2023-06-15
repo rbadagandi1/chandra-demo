22be8a6ccd737b368f09a6287b30166e71a8062c
Chandrakala M Subramanyam <csubrama@redhat.com>
2023-06-16 02:31:36 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ class Hello {
 
       System.out.println("Hello World!!"); 
 
-      System.out.println("Hello from chandra!"); 
+      System.out.println("Hello from chandra!!"); 
 
       System.out.println("Hello from kruize team!!!"); 
       System.out.println("Hi");
